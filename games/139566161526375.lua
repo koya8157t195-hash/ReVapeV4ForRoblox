88aaf17cbfdfd3feade84c063f75b07f4df5f2ac
koya <koya8157t195@gmail.com>
2026-05-05 21:05:21 -0400

    Update 139566161526375.lua

--- games/139566161526375.lua
+++ games/139566161526375.lua
@@ -2,113 +2,133 @@ local run = function(func) func() end
 local cloneref = cloneref or function(obj) return obj end
 
 local playersService = cloneref(game:GetService('Players'))
-local inputService = cloneref(game:GetService('UserInputService'))
 local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
-local collectionService = cloneref(game:GetService('CollectionService'))
 local runService = cloneref(game:GetService('RunService'))
 
-local gameCamera = workspace.CurrentCamera
 local lplr = playersService.LocalPlayer
 local vape = shared.vape
-local entitylib = vape.Libraries.entity
-local targetinfo = vape.Libraries.targetinfo
-local prediction = vape.Libraries.prediction
 
-local function notif(...)
-	return vape:CreateNotification(...)
+local function getTool()
+	return lplr.Character and lplr.Character:FindFirstChildWhichIsA('Tool')
 end
 
-for _, v in {'Reach', 'MurderMystery', 'AutoRejoin', 'Killaura', 'Swim', 'TargetStrafe', 'LongJump', 'MouseTP', 'Invisible'} do
-	vape:Remove(v)
-end
 run(function()
-local EntityCFrame
-local Killaura, Flight = {Enabled = false}, {Enabled = false}
+	local Killaura = {Enabled = false}
 	local AutoBlock = {Enabled = true}
 	local Angle = {Value = 360}
-	local Range = {Value = 16}
-	local TargetHUD = {Enabled = false}
+	local Range = {Value = 18}
 	local Wallcheck = {Enabled = false}
-	local Swing, SwingDelay = {Enabled = true}, tick()
+	local Swing = {Enabled = true}
+	
 	local AttackDelay = tick()
+	local SwingDelay = tick()
+	local target = nil
+
+	local function getTarget()
+		local entitylib = vape.Libraries.entity
+		if not (entitylib and entitylib.List) then return nil end
+		
+		local closest, maxdist = nil, Range.Value
+		for _, v in entitylib.List do
+			if not v.Targetable then continue end
+			if not entitylib.isVulnerable(v) then continue end
+			
+			local root = v.RootPart
+			if not (root and lplr.Character and lplr.Character.PrimaryPart) then continue end
+
+			local dist = (root.Position - lplr.Character.PrimaryPart.Position).Magnitude
+			if dist > maxdist then continue end
+			
+			if Angle.Value < 360 then
+				local gameCamera = workspace.CurrentCamera
+				local dot = gameCamera.CFrame.LookVector:Dot((root.Position - gameCamera.CFrame.Position).Unit)
+				if dot < math.cos(math.rad(Angle.Value / 2)) then continue end
+			end
+			
+			if Wallcheck.Enabled and entitylib.Wallcheck then
+				if entitylib.Wallcheck(lplr.Character.PrimaryPart.Position, root.Position) then continue end
+			end
+			
+			closest = v
+			maxdist = dist
+		end
+		return closest
+	end
+
 	Killaura = vape.Categories.Blatant:CreateModule({
 		Name = 'Killaura',
 		Function = function(callback)
 			if callback then
-				repeat
-					task.wait(0.03)
+				task.spawn(function()
+					repeat
+						task.wait(0.01)
+						local entitylib = vape.Libraries.entity
+						if not (entitylib and entitylib.isAlive) then continue end
 
-					if entitylib.isAlive then
-						local tool = Entity.tool.getTool(lplr)
+						local tool = getTool()
 						if tool and tool:HasTag('Sword') then
-							
-								local suc, res = pcall(function()
-									return Entity:GetClosestPlayer(Range.Value, Angle.Value, Wallcheck.Enabled)
-								end)
+							target = getTarget()
 
-								local plr
-								if suc and res then
-									plr = res
+							if target then
+								-- AutoBlock
+								replicatedStorage.Modules.Knit.Services.ToolService.RF.ToggleBlockSword:InvokeServer(AutoBlock.Enabled, tool)
+
+								-- Swing
+								if Swing.Enabled and SwingDelay < tick() then
+									SwingDelay = tick() + 0.25
+									local anim = tool:WaitForChild('Animations', 2):WaitForChild('Swing', 2)
+									if anim then
+										local hum = lplr.Character:FindFirstChildOfClass("Humanoid")
+										if hum and hum.Animator then
+											hum.Animator:LoadAnimation(anim):Play()
+										end
+									end
+
+									pcall(function()
+										local deps = shared.Dependencies
+										if deps and deps.Controllers and deps.Controllers.Viewmodel then
+											if setthreadidentity then setthreadidentity(2) end
+											deps.Controllers.Viewmodel:PlayAnimation(tool.Name)
+											if setthreadidentity then setthreadidentity(8) end
+										end
+									end)
 								end
 
-								if plr and Entity.isAlive(plr) then
-									EntityCFrame = CFrame.lookAt(lplr.Character.PrimaryPart.Position, Vector3.new(plr.Character.PrimaryPart.Position.X, lplr.Character.PrimaryPart.Position.Y, plr.Character.PrimaryPart.Position.Z))
-								
-									ReplicatedStorage.Modules.Knit.Services.ToolService.RF.ToggleBlockSword:InvokeServer(AutoBlock.Enabled, tool)
-									if Swing.Enabled and SwingDelay < tick() then
-										SwingDelay = tick() + 0.25
-										lplr.Character.Humanoid.Animator:LoadAnimation(tool:WaitForChild('Animations'):WaitForChild('Swing')):Play()
-
-										if setthreadidentity then
-											setthreadidentity(2)
+								-- Attack
+								if AttackDelay < tick() then
+									AttackDelay = tick() + 0.1
+									pcall(function()
+										local deps = shared.Dependencies
+										if deps and deps.Blink and deps.Blink.item_action and deps.Blink.item_action.attack_entity then
+											local bdplr = deps.Modules.Entity.FindByCharacter(target.Character)
+											if bdplr and bdplr.Id and deps.Constants.Extra then
+												task.spawn(deps.Blink.item_action.attack_entity.fire, {
+													target_entity_id = bdplr.Id,
+													is_crit = lplr.Character.PrimaryPart.AssemblyLinearVelocity.Y < 0,
+													weapon_name = tool.Name,
+													extra = deps.Constants.Extra
+												})
+											end
 										end
-										pcall(Dependencies.Controllers.Viewmodel.PlayAnimation, Dependencies.Controllers.Viewmodel, tool.Name)
-										if setthreadidentity then
-											setthreadidentity(8)
-										end
-									end
-
-									local suc, res = pcall(function()
-										return Dependencies.Modules.Entity.FindByCharacter(plr.Character)
 									end)
-
-									local bdplr
-											if suc and bdplr and bdplr.Id then
-									if bdplr and bdplr.Id and Dependencies.Constants.Extra and AttackDelay < tick() then -- (not Dependencies.Modules.Detections.Logs.SwordH)
-										AttackDelay = tick() + 0.1
-										task.spawn(Dependencies.Blink.item_action.attack_entity.fire, {
-											target_entity_id = bdplr.Id,
-											is_crit = (AuraCrits and true) or lplr.Character.HumanoidRootPart.AssemblyLinearVelocity.Y < 0,
-											weapon_name = tool.Name,
-											extra = Dependencies.Constants.Extra
-										})
-									end
-								else
-									EntityCFrame = nil
-								
-									if Entity.isAlive(lplr) then
-										local tool = Entity.tool.getTool(lplr)
-										if tool and tool:HasTag('Sword') then
-											ReplicatedStorage.Modules.Knit.Services.ToolService.RF.ToggleBlockSword:InvokeServer(false, tool)
-										end
-									end
+								end
+							else
+								if tool then
+									replicatedStorage.Modules.Knit.Services.ToolService.RF.ToggleBlockSword:InvokeServer(false, tool)
 								end
 							end
-						end)
-					end
-				until not Killaura.Enabled
+						end
+					until not Killaura.Enabled
+				end)
 			else
-				EntityCFrame = nil
-
-				if Entity.isAlive(lplr) then
-					local tool = Entity.tool.getTool(lplr)
-					if tool and tool:HasTag('Sword') then
-						ReplicatedStorage.Modules.Knit.Services.ToolService.RF.ToggleBlockSword:InvokeServer(false, tool)
-					end
+				local tool = getTool()
+				if tool and tool:HasTag('Sword') then
+					replicatedStorage.Modules.Knit.Services.ToolService.RF.ToggleBlockSword:InvokeServer(false, tool)
 				end
 			end
 		end
 	})
+
 	AutoBlock = Killaura:CreateToggle({
 		Name = 'AutoBlock',
 		Enabled = true
