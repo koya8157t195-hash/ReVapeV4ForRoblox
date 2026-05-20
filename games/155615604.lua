cfc64bb250097202f62bb5eaa499ca624075d9d7
koya <koya8157t195@gmail.com>
2026-05-20 18:55:47 -0400

    Update 155615604.lua

--- games/155615604.lua
+++ games/155615604.lua
@@ -44,6 +44,273 @@ local function notif(...)
 	return vape:CreateNotification(...)
 end
 
+for _, v in {'AutoClicker', 'Reach', 'MurderMystery', 'AutoRejoin', 'Killaura', 'SilentAim', 'LongJump', 'MouseTP'} do
+	vape:Remove(v)
+end
+
+run(function()
+    local SilentAim
+    local Target
+    local Mode
+    local IgnoredScripts
+    local Range
+    local HitChance
+    local HeadshotChance
+    local AutoFire
+    local AutoFireShootDelay
+    local AutoFireMode
+    local AutoFirePosition
+    local Wallbang
+    local CircleColor
+    local CircleTransparency
+    local CircleFilled
+    local CircleObject
+    local RaycastWhitelist = RaycastParams.new()
+    RaycastWhitelist.FilterType = Enum.RaycastFilterType.Include
+    local fireoffset, rand, delayCheck = CFrame.identity, Random.new(), tick()
+    local oldnamecall
+    local mouseClicked
+
+    local function getTarget(origin, obj)
+        if rand.NextNumber(rand, 0, 100) > (AutoFire.Enabled and 100 or HitChance.Value) then return end
+        local targetPart = (rand.NextNumber(rand, 0, 100) < (AutoFire.Enabled and 100 or HeadshotChance.Value)) and 'Head' or 'RootPart'
+        local ent = entitylib['Entity'..Mode.Value]({
+            Range = Range.Value,
+            Wallcheck = Target.Walls.Enabled and (obj or true) or nil,
+            Part = targetPart,
+            Origin = origin,
+            Players = Target.Players.Enabled,
+            NPCs = Target.NPCs.Enabled
+        })
+
+        if ent then
+            targetinfo.Targets[ent] = tick() + 1
+        end
+
+        return ent, ent and ent[targetPart], origin
+    end
+
+    SilentAim = vape.Categories.Combat:CreateModule({
+        Name = 'SilentAim',
+        Function = function(callback)
+            if CircleObject then
+                CircleObject.Visible = callback and Mode.Value == 'Mouse'
+            end
+            if callback then
+                oldnamecall = hookmetamethod(game, '__namecall', function(...)
+                    if getnamecallmethod() ~= 'Raycast' then
+                        return oldnamecall(...)
+                    end
+                    if checkcaller() then
+                        return oldnamecall(...)
+                    end
+
+                    local calling = getcallingscript()
+                    if calling then
+                        local list = #IgnoredScripts.ListEnabled > 0 and IgnoredScripts.ListEnabled or {'ControlScript', 'ControlModule'}
+                        if table.find(list, tostring(calling)) then
+                            return oldnamecall(...)
+                        end
+                    end
+
+                    local self, args = ..., {select(2, ...)}
+                    local ent, targetPart, origin = getTarget(args[1])
+                    if not ent then return oldnamecall(self, unpack(args)) end
+
+                    args[2] = CFrame.lookAt(origin, targetPart.Position).LookVector * args[2].Magnitude
+                    if Wallbang.Enabled then
+                        RaycastWhitelist.FilterDescendantsInstances = {targetPart}
+                        args[3] = RaycastWhitelist
+                    end
+
+                    return oldnamecall(self, unpack(args))
+                end)
+
+                repeat
+                    if CircleObject then
+                        CircleObject.Position = inputService:GetMouseLocation()
+                    end
+
+                    if AutoFire.Enabled then
+                        local origin = AutoFireMode.Value == 'Camera' and gameCamera.CFrame or entitylib.isAlive and entitylib.character.RootPart.CFrame or CFrame.identity
+                        local ent = entitylib['Entity'..Mode.Value]({
+                            Range = Range.Value,
+                            Wallcheck = Target.Walls.Enabled or nil,
+                            Part = 'Head',
+                            Origin = (origin * fireoffset).Position,
+                            Players = Target.Players.Enabled,
+                            NPCs = Target.NPCs.Enabled
+                        })
+
+                        if mouse1click and (isrbxactive or iswindowactive)() then
+                            if ent and canClick() then
+                                if delayCheck < tick() then
+                                    if mouseClicked then
+                                        mouse1release()
+                                        delayCheck = tick() + AutoFireShootDelay.Value
+                                    else
+                                        mouse1press()
+                                    end
+                                    mouseClicked = not mouseClicked
+                                end
+                            else
+                                if mouseClicked then
+                                    mouse1release()
+                                end
+                                mouseClicked = false
+                            end
+                        end
+                    end
+
+                    task.wait()
+                until not SilentAim.Enabled
+            else
+                if oldnamecall then
+                    hookmetamethod(game, '__namecall', oldnamecall)
+                end
+                oldnamecall = nil
+            end
+        end,
+        Tooltip = 'Silently adjusts your aim towards the enemy using Raycast'
+    })
+    Target = SilentAim:CreateTargets({Players = true})
+    Mode = SilentAim:CreateDropdown({
+        Name = 'Mode',
+        List = {'Mouse', 'Position'},
+        Function = function(val)
+            if CircleObject then
+                CircleObject.Visible = SilentAim.Enabled and val == 'Mouse'
+            end
+        end,
+        Tooltip = 'Mouse - Checks for entities near the mouses position\nPosition - Checks for entities near the local character'
+    })
+    IgnoredScripts = SilentAim:CreateTextList({Name = 'Ignored Scripts'})
+    Range = SilentAim:CreateSlider({
+        Name = 'Range',
+        Min = 1,
+        Max = 1000,
+        Default = 150,
+        Function = function(val)
+            if CircleObject then
+                CircleObject.Radius = val
+            end
+        end,
+        Suffix = function(val)
+            return val == 1 and 'stud' or 'studs'
+        end
+    })
+    HitChance = SilentAim:CreateSlider({
+        Name = 'Hit Chance',
+        Min = 0,
+        Max = 100,
+        Default = 85,
+        Suffix = '%'
+    })
+    HeadshotChance = SilentAim:CreateSlider({
+        Name = 'Headshot Chance',
+        Min = 0,
+        Max = 100,
+        Default = 65,
+        Suffix = '%'
+    })
+    AutoFire = SilentAim:CreateToggle({
+        Name = 'AutoFire',
+        Function = function(callback)
+            AutoFireShootDelay.Object.Visible = callback
+            AutoFireMode.Object.Visible = callback
+            AutoFirePosition.Object.Visible = callback
+        end
+    })
+    AutoFireShootDelay = SilentAim:CreateSlider({
+        Name = 'Next Shot Delay',
+        Min = 0,
+        Max = 1,
+        Decimal = 100,
+        Visible = false,
+        Darker = true,
+        Suffix = function(val)
+            return val == 1 and 'second' or 'seconds'
+        end
+    })
+    AutoFireMode = SilentAim:CreateDropdown({
+        Name = 'Origin',
+        List = {'RootPart', 'Camera'},
+        Visible = false,
+        Darker = true,
+        Tooltip = 'Determines the position to check for before shooting'
+    })
+    AutoFirePosition = SilentAim:CreateTextBox({
+        Name = 'Offset',
+        Function = function()
+            local suc, res = pcall(function()
+                return CFrame.new(unpack(AutoFirePosition.Value:split(',')))
+            end)
+            if suc then fireoffset = res end
+        end,
+        Default = '0, 0, 0',
+        Visible = false,
+        Darker = true
+    })
+    Wallbang = SilentAim:CreateToggle({Name = 'Wallbang'})
+    SilentAim:CreateToggle({
+        Name = 'Range Circle',
+        Function = function(callback)
+            if callback then
+                CircleObject = Drawing.new('Circle')
+                CircleObject.Filled = CircleFilled.Enabled
+                CircleObject.Color = Color3.fromHSV(CircleColor.Hue, CircleColor.Sat, CircleColor.Value)
+                CircleObject.Position = vape.gui.AbsoluteSize / 2
+                CircleObject.Radius = Range.Value
+                CircleObject.NumSides = 100
+                CircleObject.Transparency = 1 - CircleTransparency.Value
+                CircleObject.Visible = SilentAim.Enabled and Mode.Value == 'Mouse'
+            else
+                pcall(function()
+                    CircleObject.Visible = false
+                    CircleObject:Remove()
+                end)
+            end
+            CircleColor.Object.Visible = callback
+            CircleTransparency.Object.Visible = callback
+            CircleFilled.Object.Visible = callback
+        end
+    })
+    CircleColor = SilentAim:CreateColorSlider({
+        Name = 'Circle Color',
+        Function = function(hue, sat, val)
+            if CircleObject then
+                CircleObject.Color = Color3.fromHSV(hue, sat, val)
+            end
+        end,
+        Darker = true,
+        Visible = false
+    })
+    CircleTransparency = SilentAim:CreateSlider({
+        Name = 'Transparency',
+        Min = 0,
+        Max = 1,
+        Decimal = 10,
+        Default = 0.5,
+        Function = function(val)
+            if CircleObject then
+                CircleObject.Transparency = 1 - val
+            end
+        end,
+        Darker = true,
+        Visible = false
+    })
+    CircleFilled = SilentAim:CreateToggle({
+        Name = 'Circle Filled',
+        Function = function(callback)
+            if CircleObject then
+                CircleObject.Filled = callback
+            end
+        end,
+        Darker = true,
+        Visible = false
+    })
+end)
+
 run(function()
     local GunModifications
     local charConnection
